31e3a7f47e4ead702ff19c3d64cf26dd83eda8b9
Felix Hennig <mail@felixhennig.com>
2015-02-06 21:06:34 +0100

    changed logic and tests

--- src/floodr/logic.clj
+++ src/floodr/logic.clj
@@ -2,44 +2,74 @@
   (:require [clojure.set :as set])
   (:gen-class))
 
-(defstruct node :x :y :parent)
+; :parents - a map node to node
+; :neighbors - a map node to set of nodes
+; :colors - a map node to color
+(defstruct world :parents :neighbors :colors)
 
-(defstruct cluster :color :nodes)
+(def empty-world (struct world {} {} {}))
+
+(defn add-node
+  [w node node-parent node-neighbors node-color]
+  (struct world
+          (assoc-in (get w :parents) [node] node-parent)
+          (assoc-in (get w :neighbors) [node] node-neighbors)
+          (assoc-in (get w :colors) [node] node-color)))
+
+
+
+(defn cluster 
+  "returns the cluster that the node is a member of"
+  [world node]
+  (let [p (get (get world :parents) node)]
+    (if (= node p)
+      node
+      (cluster world p))))
+
+(defn neighbors 
+  "returns the neighbor clusters of the cluster"
+  [world node]
+  (set (map #(cluster world %)
+            (get (get world :neighbors) (cluster world node)))))
+  
+(defn merge-clusters
+  "returns a new world where c1 and c2 are merged"
+  [w n1 n2]
+  (let [c1 (cluster w n1) c2 (cluster w n2)]
+    (struct world
+            (assoc-in (get w :parents) [c1] c2)
+            (assoc-in (get w :neighbors) [c2]
+                      (set/difference (set/union (neighbors w c1) (neighbors w c2)) #{c1 c2}))
+            (get w :colors))))
+
+(defn color
+  "gets the color of a cluster"
+  [w node]
+  (get (get w :colors) (cluster w node)))
 
 (defn adjacent?
-  "Checks if two nodes are next to each other"
-  [p1 p2]
-  (let [p1x (get p1 :x) p1y (get p1 :y)
-        p2x (get p2 :x) p2y (get p2 :y)]
-    (or (and (= p1x p2x)
-             (or (= p1y (+ p2y 1))
-                 (= p1y (- p2y 1))))
-        (and (= p1y p2y)        
-             (or (= p1x (+ p2x 1))
-                 (= p1x (- p2x 1)))))))
+  "determines if two clusters are next to each other"
+  [w n1 n2]
+  (contains? (neighbors w (cluster w n1)) (cluster w n2)))
 
-(defn sets-adjacent?
-  "Checks if two sets 'touch'"
-  [set1 set2]
-  (some (fn [node]
-          (some #(adjacent? node %) set1))
-        set2))
+(defn gen-neighbors
+  [w h node]
+  (let [up (- node w) down (+ node w)
+        left (- node 1) right (+ node 1)]
+    (set/union (when (>= up 0) #{up})
+               (when (< down (* w h)) #{down})
+               (when (and (= (quot node w) (quot left w))
+                          (>= left 0)) #{left})
+               (when (and (= (quot node w) (quot right w))
+                          (< right (* w h))) #{right}))))
 
-(defn same-color? [cluster1 cluster2]
-  (= (get cluster1 :color)
-     (get cluster2 :color)))
+(def colors '(:red :green :blue :yellow :cyan :magenta))
 
-(defn mergeable? [cluster1 cluster2]
-  (and (same-color? cluster1 cluster2)
-       (sets-adjacent? (get cluster1 :nodes)
-                       (get cluster2 :nodes))))
-
-(defn merge-all [cs]
-  (when (> (count cs) 1)
-    (let [c1 (first cs) c2 (second cs)]
-    (if (mergeable? c1 c2)
-      (recur (cons (struct cluster (get c1 :color)
-                           (set/union (get c1 :nodes)
-                                      (get c2 :nodes)))
-                   (rest (rest cs))))
-      (recur (rest cs))))))
+(defn gen-rand-world
+  [width height]
+  (let [nodes (range (* width height))
+        add (fn [world node]
+              (add-node world node node
+                        (gen-neighbors width height node)
+                        (rand-nth colors)))]
+    (reduce add empty-world nodes)))
--- test/floodr/logic_test.clj
+++ test/floodr/logic_test.clj
@@ -2,52 +2,37 @@
   (:require [clojure.test :refer :all]
             [floodr.logic :refer :all]))
 
-;               (3, 3)
-; (2, 1) (2, 2)
-; (1, 1) (2, 1)
+(deftest everything-syntactically-correct
+  (let [w (gen-rand-world 3 3)]
+    (cluster w 1)
+    (neighbors w 1)
+    (merge-clusters w 1 2)
+    (color w 1)
+    (adjacent? w 1 2)))
 
-(def p11 (struct point 1 1))
-(def p12 (struct point 1 2))
-(def p22 (struct point 2 2))
-(def p33 (struct point 3 3))
-(def p21 (struct point 2 1))
+(deftest test-add-node
+  (is (= (add-node empty-world 1 2 #{3 4} :red)
+         (struct world {1 2} {1 #{3 4}} {1 :red}))))
 
-(def set1 #{p11 p12})
-(def set2 #{p21 p22})
-(def set12 #{p11 p12 p21 p22})
-(def set3 #{p33})
-(def empty-set #{})
+(def tw (struct world
+                {8 7, 7 7, 6 6, 5 5, 4 3, 3 1, 2 2, 1 1, 0 0}
+                {8 #{7 5}, 7 #{1 6 5}, 6 #{7 3}, 5 #{4 2 8}, 4 #{7 1 3 5}, 3 #{0 7 1 6 5}, 2 #{1 5}, 1 #{0 7 6 2 5}, 0 #{1 3}}
+                {8 :green, 7 :red, 6 :cyan, 5 :green, 4 :cyan, 3 :magenta, 2 :blue, 1 :green, 0 :cyan}))
+; clusters: (1 3 4) (7 8) singles ...
 
-(def cl1r (struct cluster :red set1))
-(def cl1g (struct cluster :green set1))
-(def cl2r (struct cluster :red set2))
-(def cl2g (struct cluster :green set2))
-(def cl12r (struct cluster :red set12))
-(def cl3r (struct cluster :red set3))
-(def cl3g (struct cluster :green set3))
+(deftest test-cluster
+  (is (= (cluster tw 1) (cluster tw 1)))
+  (is (= (cluster tw 3) (cluster tw 1)))
+  (is (= (cluster tw 4) (cluster tw 1)))
+  (is (= (cluster tw 7) (cluster tw 8)))
+  (is (= (cluster tw 8) (cluster tw 8))))
 
+(deftest test-neighbors
+  (is (contains? (neighbors tw 4) (cluster tw 0)))
+  (is (contains? (neighbors tw 4) (cluster tw 2)))
+  (is (contains? (neighbors tw 4) (cluster tw 5)))
+  (is (contains? (neighbors tw 4) (cluster tw 6)))
+  (is (contains? (neighbors tw 4) (cluster tw 7)))
+  (is (contains? (neighbors tw 4) (cluster tw 8)))
+  (is (contains? (neighbors tw 0) (cluster tw 1))))
 
-(deftest test-adjacent?
-  (is (adjacent? p11 p12) "up")
-  (is (adjacent? p12 p11) "down")
-  (is (adjacent? p22 p12) "left")
-  (is (adjacent? p12 p22) "right")
-  (is (not (adjacent? p11 p22)) "not adjacent"))
-      
-(deftest test-sets-adjacent?
-  (is (sets-adjacent? set1 set2) "should be adjacent")
-  (is (not (sets-adjacent? set1 set3)) "should not be adjacent")
-  (is (not (sets-adjacent? set1 empty-set)) "empty set should not be adjacent"))
-
-(deftest test-same-color?
-  (is (same-color? cl1r cl2r) "both red")
-  (is (not (same-color? cl1r cl1g)) "one red, one green"))
-
-(deftest test-mergeable?
-  (is (mergeable? cl1r cl2r) "adjacent and same color")
-  (is (not (mergeable? cl1r cl2g)) "not same color")
-  (is (not (mergeable? cl1r cl3r)) "not adjacent")
-  (is (not (mergeable? cl1r cl3g)) "not adjacent and not same color"))
-
-(deftest test-merge-all
-  (is (= #{cl12r cl3r} (merge-all #{cl1r cl2r cl3r})) "1 and 2 can be merged, 3 not"))
